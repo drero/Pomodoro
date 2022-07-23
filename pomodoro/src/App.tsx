b37573d4e04b4a8c65c77179c30e8fe3333e04bc
Andre <dreroips@gmail.com>
2022-07-23 16:01:35 -0300

    css: Estilos Globais - Styled-Components

--- pomodoro/src/App.tsx
+++ pomodoro/src/App.tsx
@@ -1,17 +1,19 @@
 import { ThemeProvider } from 'styled-components'
 import { Button } from './components/Button';
 
+import { GlobalStyle } from './styles/global';
 import { defaultTheme } from './styles/themes/default';
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-    <h1>Hello World</h1>
       <Button variant="primary" textColor='grey' />
       <Button variant="secondary" textColor='white' />
       <Button variant="success" textColor='black' />
       <Button variant="danger" />
       <Button />
+
+      <GlobalStyle />
     </ThemeProvider>
   )
 }
