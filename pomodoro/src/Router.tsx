f6296fef3b9fdbfe802c97eef8721c867e1d3c68
Andre <dreroips@gmail.com>
2022-07-28 14:24:05 -0300

    criação e estilização da Home

--- pomodoro/src/Router.tsx
+++ pomodoro/src/Router.tsx
@@ -3,7 +3,7 @@ import { Routes, Route } from 'react-router-dom'
 import { DefaultLayout } from './layouts/DefaultLayout/index'
 
 import { History } from './pages/History'
-import { Home } from './pages/Home'
+import { Home } from './pages/Home/index'
 
 export function Router() {
   return (
