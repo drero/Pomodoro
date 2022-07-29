df0010e1f4babcaf1776b556017cf5aec4f8ea7c
Andre <dreroips@gmail.com>
2022-07-29 15:06:19 -0300

    Estilização do botão submit

--- pomodoro/src/pages/Home/index.tsx
+++ pomodoro/src/pages/Home/index.tsx
@@ -4,6 +4,7 @@ import {
   FormContainer,
   HomeContainer,
   Separator,
+  StartCountDownButton,
 } from './styles'
 
 export function Home() {
@@ -27,10 +28,10 @@ export function Home() {
           <span>0</span>
         </CountdownContainer>
 
-        <button type="submit">
+        <StartCountDownButton disabled type="submit">
           <Play size={24} />
           Começar
-        </button>
+        </StartCountDownButton>
       </form>
     </HomeContainer>
   )
