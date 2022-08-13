523e5367a22c07106bfca6a564cef08ba6b534e2
Andre <dreroips@gmail.com>
2022-08-12 21:14:58 -0300

    code: hookForm validação com Zod

--- pomodoro/src/pages/Home/index.tsx
+++ pomodoro/src/pages/Home/index.tsx
@@ -1,5 +1,8 @@
 import { Play } from 'phosphor-react'
 import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import * as zod from 'zod'
+
 import {
   CountdownContainer,
   FormContainer,
@@ -10,8 +13,18 @@ import {
   TaskInput,
 } from './styles'
 
+const newCycleFormValidationState = zod.object({
+  task: zod.string().min(1, 'Informe a tarefa'),
+  minutesAmount: zod
+    .number()
+    .min(5, 'O ciclo precisa ser de no minimo de 5 minutos')
+    .max(60, 'O ciclo precisa ser de no máximo 60 minutos'),
+})
+
 export function Home() {
-  const { register, handleSubmit, watch } = useForm()
+  const { register, handleSubmit, watch } = useForm({
+    resolver: zodResolver(newCycleFormValidationState),
+  })
 
   function handleCreateNewCycle(data: any) {
     console.log(data)
